da8bcda8481a2b8ca261dcef083cae69ecc8bcb4
Vladislav Zakrevskiy <vladzakrevski@MacBook-Pro-3.local>
2025-05-29 11:07:41 +0300

    ассистент +

--- src/bot/scenes/profiles/Admin.scene.ts
+++ src/bot/scenes/profiles/Admin.scene.ts
@@ -1,29 +1,28 @@
 import { Ctx, Action, Update } from 'nestjs-telegraf';
-import { UserService } from 'src/user/user.service';
 import { SessionSceneContext } from 'src/bot/types/Scene';
-import { EmployeeLevel } from '@prisma/client';
+import { OrderProcess } from '@prisma/client';
 import { getDefaultText } from 'src/core/helpers/getDefaultText';
 import { RedisService } from 'src/core/redis/redis.service';
 import { getRedisKeys } from 'src/core/redis/redisKeys';
-import { getTelegramImage } from 'src/core/helpers/getTelegramImage';
 import { ListManager } from 'src/bot/template/ListManager';
+import { OrderService } from 'src/order/order.service';
+import { CallbackQuery } from 'telegraf/typings/core/types/typegram';
 
 @Update()
 export class AdminProfileService {
   constructor(
-    private readonly userService: UserService,
+    private readonly orderService: OrderService,
     private redis: RedisService,
   ) {}
 
-  // ListManager and some data
-  async getListManager(ctx: SessionSceneContext) {
+  async getOrdersListManager(ctx: SessionSceneContext, process_type: OrderProcess = 'DONE') {
     const currentIndex = Number(
-      await this.redis.get(getRedisKeys('currentIndex_admin', '', ctx.chat.id)),
+      await this.redis.get(getRedisKeys('currentIndex_bossorders', process_type, ctx.chat.id)),
     );
-    const users = await this.userService.findUserByRole(EmployeeLevel.EMPLOYEE);
+    const orders = await this.orderService.findManyByParameter({ process: [process_type] });
     const listManager = new ListManager(
       this.redis,
-      users,
+      orders,
       {
         extraButtons: [
           [
@@ -31,61 +30,55 @@ export class AdminProfileService {
             // @ts-ignore
             {
               text: 'Редактировать',
-              web_app: (user) => ({ url: `${process.env.WEBAPP_URL}/updateUser/${user.id}` }),
+              web_app: (order) => ({ url: `${process.env.WEBAPP_URL}/updateOrder/${order.id}` }),
+            },
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            {
+              text: 'Найти 🔎',
+              web_app: () => ({ url: `${process.env.WEBAPP_URL}/find` }),
             },
           ],
         ],
         getText: (order) => getDefaultText(order, 'char'),
-        getImage: async (user) => (await getTelegramImage(ctx, user.tg_user_id)).toString(),
+        getImage: async (order) => order.image_urls[0],
       },
       ctx,
-      '',
-      'currentIndex_admin',
+      process_type,
+      'currentIndex_bossorders',
     );
 
-    return { listManager, currentIndex, users };
+    return { listManager, currentIndex, orders };
+  }
+  // OrderList
+  @Action(/^next__currentIndex_bossorders_(DONE|IN_WORK|RETURN)$/)
+  public async handleOrderNext(@Ctx() ctx: SessionSceneContext): Promise<void> {
+    const prefix = (ctx.callbackQuery as CallbackQuery.DataQuery).data.split(
+      '_',
+    )?.[4] as OrderProcess;
+    const { currentIndex, listManager, orders } = await this.getOrdersListManager(ctx, prefix);
+
+    if (currentIndex < orders.length - 1) {
+      await this.redis.set(
+        getRedisKeys('currentIndex_bossorders', listManager.prefix, ctx.chat.id),
+        currentIndex + 1,
+      );
+      await listManager.editMessage();
+    } else {
+      await ctx.answerCbQuery('Нет следующего элемента');
+    }
   }
 
-  // Profile
-  async handleProfile(ctx: SessionSceneContext) {
-    const user_id = await this.redis.get(getRedisKeys('user_id', ctx.chat.id));
-    const user = await this.userService.findUserById(user_id, true);
-
-    const photo_file_id = (await ctx.telegram.getUserProfilePhotos(user.tg_user_id, 0, 1))
-      .photos[0][2].file_id;
-    const photo_url = await ctx.telegram.getFileLink(photo_file_id);
-
-    ctx.sendPhoto(
-      { url: photo_url.toString() },
-      {
-        caption: getDefaultText(user, 'new'),
-        parse_mode: 'MarkdownV2',
-        reply_markup: {
-          inline_keyboard: [[{ callback_data: 'admin_employees', text: 'Сотрудники' }]],
-        },
-      },
-    );
-  }
-
-  // List
-  @Action('next__currentIndex_admin_')
-  public async handleNext(@Ctx() ctx: SessionSceneContext): Promise<void> {
-    const { currentIndex, listManager } = await this.getListManager(ctx);
-
-    await this.redis.set(
-      getRedisKeys('currentIndex_admin', listManager.prefix, ctx.chat.id),
-      currentIndex + 1,
-    );
-    await listManager.editMessage();
-  }
-
-  @Action('prev__currentIndex_admin_')
-  public async handlePrev(@Ctx() ctx: SessionSceneContext): Promise<void> {
-    const { currentIndex, listManager } = await this.getListManager(ctx);
+  @Action(/^prev__currentIndex_bossorders_(DONE|IN_WORK|RETURN)$/)
+  public async handleOrderPrev(@Ctx() ctx: SessionSceneContext): Promise<void> {
+    const prefix = (ctx.callbackQuery as CallbackQuery.DataQuery).data.split(
+      '_',
+    )?.[4] as OrderProcess;
+    const { currentIndex, listManager } = await this.getOrdersListManager(ctx, prefix);
 
     if (currentIndex > 0) {
       await this.redis.set(
-        getRedisKeys('currentIndex_admin', listManager.prefix, ctx.chat.id),
+        getRedisKeys('currentIndex_bossorders', listManager.prefix, ctx.chat.id),
         currentIndex - 1,
       );
       await listManager.editMessage();
@@ -94,12 +87,36 @@ export class AdminProfileService {
     }
   }
 
-  @Action('admin_employees')
-  async sendDoneOrders(@Ctx() ctx: SessionSceneContext) {
-    const { listManager, users } = await this.getListManager(ctx);
+  @Action('boss_done_orders')
+  async doneOrders(@Ctx() ctx: SessionSceneContext) {
+    const { listManager, orders } = await this.getOrdersListManager(ctx, OrderProcess.DONE);
 
-    if (users.length === 0) {
-      await ctx.reply('Сотрудников нет(');
+    if (orders.length === 0) {
+      await ctx.reply('Выполненных заказов нет(');
+      return;
+    }
+
+    listManager.sendInitialMessage();
+  }
+
+  @Action('boss_work_orders')
+  async workOrders(@Ctx() ctx: SessionSceneContext) {
+    const { listManager, orders } = await this.getOrdersListManager(ctx, OrderProcess.IN_WORK);
+
+    if (orders.length === 0) {
+      await ctx.reply('Заказов в работе нет(');
+      return;
+    }
+
+    listManager.sendInitialMessage();
+  }
+
+  @Action('boss_return_orders')
+  async returnOrders(@Ctx() ctx: SessionSceneContext) {
+    const { listManager, orders } = await this.getOrdersListManager(ctx, OrderProcess.RETURN);
+
+    if (orders.length === 0) {
+      await ctx.reply('Возвратов нет(');
       return;
     }
 
