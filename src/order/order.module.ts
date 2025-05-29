498dbc23ef694f6908a48fac33ad87c056d57967
Vladislav Zakrevskiy <vladzakrevski@MacBook-Pro-3.local>
2025-05-29 11:29:17 +0300

    ассистент +

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,10 +5,11 @@ import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from 'src/auth/jwt/jwt.service';
 import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
 
 @Module({
   imports: [],
-  controllers: [OrderCotroller],
+  controllers: [OrderCotroller, CategoryController],
   providers: [OrderService, PrismaService, UserService, JwtService, CategoryService],
 })
 export class OrderModule {}
